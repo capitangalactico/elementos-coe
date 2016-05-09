7ce60795c8fd7174931de5576b246e4fbe7d0ab3
JuanDanielCR <castilloreyesjuan@gmail.com>
2016-05-08 20:46:06 -0500

    UI lista

--- js/actividad.js
+++ js/actividad.js
@@ -22,13 +22,85 @@
 		"puntos":0
 	}
 
-	tablero[1] = pregunta;
+	links_suaves();
+	verificar_respuestas(1, 2);
+	mostrar_pregunta(1, 50, "¿Tona es gay?", 2);
+	
 
-	$('.prueba').append('<h1>'+equipo.nombre+'<h1>');
 })();
+
+
+
 
 function crear_tablero(){}
 function calcular_ganador(equipo1, equipo2){} //recibe las puntuaciones y muestra un ganador
 function contestar_pregunta(pregunta){} //valida las respuestas del equipo y muestra correcto/incorrecto
 function elegir_pregunta(pregunta){} //pintar la nueva interfaz con las respuestas
 function disable_pregunta(){} //repinta los estilos para una pregunta contestada correctamente
+
+function links_suaves(){
+
+	//funcion visual no afecta el juego
+
+	$('a[href^="#"]').on('click',function (e) {
+	    e.preventDefault();
+	    var target = this.hash;
+	    var $target = $(target);
+	    $('html, body').stop().animate({
+	        'scrollTop': $target.offset().top
+	    }, 1200, 'swing', function () {
+	        window.location.hash = target;
+	    });
+	});
+	/*Animacion inicial*/
+	$( ".intro" ).animate({
+		backgroundPosition:"200px"
+	}, 5000);
+	$( ".intro .uno" ).animate({
+		opacity:"1"
+	}, 2000,function(){
+	$( ".intro .dos" ).animate({
+	 	opacity:"1"
+	}, 2000, function(){
+	$( ".intro .tres" ).animate({
+	 	opacity:"1"
+	  }, 2000);
+	});
+	});
+}
+
+function mostrar_pregunta(id_pregunta, puntos, contenido_pregunta, respuestas){
+	$("html").jPopup({
+		heading:"Pregunta "+id_pregunta+" - "+puntos+"pts",
+		paragraph: ""+contenido_pregunta,
+		buttonClass : "btn btn-info",
+		userContent:"<label><input type='checkbox' id='cbox1' value='first_checkbox'>Tonatihu</label><br><input type='checkbox' id='cbox2' value''second_checkbox'> <label for='cbox2'>Juan Daniel</label><br><button class='popup-close-button btn btn-danger' id='boton"+id_pregunta+"'>Contestar</button>",
+		initThrough : function () {
+			$(".btn-init-pop").click(function(e) {
+				e.preventDefault();
+				$.fn.jPopup.openPopup();
+			});
+		}
+	});
+}
+
+function verificar_respuestas(id_pregunta, respuestas){
+	$("boton"+id_pregunta+"").click(function(){
+		var correcto=true;
+		/*AQUI VA TU MAGIA PARA VER SI ESTA CORRECTO*/
+		if(correcto){
+			swal(
+		  	'Bien hecho',
+		  	'Continua así',
+		  	'success'
+			)
+		}else{
+			swal(
+		  	'Bien hecho',
+		  	'Continua así',
+		  	'error'
+			)
+		}
+		/*DESPUES DE VERIFICAR AQUI SE LLAMARIA A UN FUNCION QUE AUMENTE LOS PUNTOS DEL EQUIPO Y LOS ACTUALIZAE EN LA UI*/
+	});
+}
